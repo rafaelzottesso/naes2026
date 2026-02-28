b274635bd25f23934debd0f6cff91632c693e10f
Professor <rafael.zottesso@ifpr.edu.br>
2026-02-27 21:10:05 -0300

    feat(website): configurar arquivos estaticos e templates
    Configura naes2026/settings.py e naes2026/urls.py para servir a interface.
    Adiciona static/css/estilo.css e os templates iniciais do app website.

---
+++ website/urls.py
@@ -0,0 +1,8 @@
+
+from django.urls import path
+from .views import IndexView, ContatoView
+
+urlpatterns = [
+    path("", IndexView.as_view(), name="index"),
+    path("contato/", ContatoView.as_view(), name="contato"),
+]
