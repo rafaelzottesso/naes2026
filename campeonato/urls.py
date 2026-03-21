dd47f425d8f0515d2466049fffae9788a542f0c1
Professor <rafael.zottesso@ifpr.edu.br>
2026-03-20 21:04:26 -0300

    Criação das views CreateView para inserir registros

--- campeonato/urls.py
+++ campeonato/urls.py
@@ -1,7 +1,10 @@
 
 from django.urls import path
-# from .views import Nomedaview
+from .views import CampusCreate, ModalidadeCreate, EtapaCreate, CampeonatoCreate
 
 urlpatterns = [
-    # path("rotadaview/", Nomedaview.as_view(), name="nome"),
+    path('cadastrar/campus/', CampusCreate.as_view(), name='campus-create'),
+    path('cadastrar/modalidade/', ModalidadeCreate.as_view(), name='modalidade-create'),
+    path('cadastrar/etapa/', EtapaCreate.as_view(), name='etapa-create'),
+    path('cadastrar/campeonato/', CampeonatoCreate.as_view(), name='campeonato-create'),
 ]
--- campeonato/views.py
+++ campeonato/views.py
@@ -1,3 +1,55 @@
-from django.shortcuts import render
+from django.views.generic import CreateView
+# Buscar a rota da url pelo name dela (urls.py)
+from django.urls import reverse_lazy
 
-# Create your views here.
+from .models import Campus, Jogador, Modalidade, Etapa, Campeonato, Inscricao, Jogo
+
+
+class CampusCreate(CreateView):
+    model = Campus
+    fields = ['nome']
+    template_name = 'campeonato/form.html'
+    success_url = reverse_lazy('index')
+    extra_context = {
+        'titulo': 'Cadastro de Campus',
+        'botao': 'Criar Campus'
+    }
+
+#########################################################
+
+
+class ModalidadeCreate(CreateView):
+    model = Modalidade
+    fields = ['nome']
+    template_name = 'campeonato/form.html'
+    success_url = reverse_lazy('index')
+    extra_context = {
+        'titulo': 'Cadastro de Modalidade',
+        'botao': 'Criar Modalidade'
+    }
+
+#########################################################
+
+
+class EtapaCreate(CreateView):
+    model = Etapa
+    fields = ['nome', 'sequencia', 'quantidade_jogos']
+    template_name = 'campeonato/form.html'
+    success_url = reverse_lazy('index')
+    extra_context = {
+        'titulo': 'Cadastro de Etapa',
+        'botao': 'Criar Etapa'
+    }
+
+##########################################################
+
+
+class CampeonatoCreate(CreateView):
+    model = Campeonato
+    fields = ['nome', 'campus', 'data', 'data_inscricao', 'modalidades', 'cadastrado_por']
+    template_name = 'campeonato/form.html'
+    success_url = reverse_lazy('index')
+    extra_context = {
+        'titulo': 'Cadastro de Campeonato',
+        'botao': 'Criar Campeonato'
+    }
